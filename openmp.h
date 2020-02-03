c45d1ff7349deb3ade42b634adc8ef2c9ed18c7b
Ed Younis <edyounis123@gmail.com>
2020-02-02 23:41:26 -0800

    Initial Commit

---
+++ openmp.h
@@ -0,0 +1,12 @@
+#ifndef __CS267_SERIAL_H__
+#define __CS267_SERIAL_H__
+
+#include "common.h"
+
+
+void simulate_one_step ( particle_t* parts, int num_parts, double size )
+{
+	// Write this function
+}
+
+#endif
