f309a4d8631937d1e7cade5d40a2ba5683349a53
RichZhou1999 <373072096@qq.com>
2023-02-12 23:39:45 -0800

    modified the way to update the bin of particles

--- serial.cpp
+++ serial.cpp
@@ -3,10 +3,9 @@
 #include <set>
 #include <unordered_map>
 #include <iostream>
+#include <bits/stdc++.h>
 
-#define EPS  0.001
-
-std::unordered_map<int, std::set<int>> bins;
+std::unordered_map<int, std::unordered_set<particle_t *>> bins;
 double bin_size = 3*cutoff;
 int lda;
 
@@ -30,10 +29,24 @@ void apply_force(particle_t& particle, particle_t& neighbor) {
     particle.ay += coef * dy;
 }
 
+int calculate_bin_number(double x, double y, double size, double bin_size, int lda){
+    double quotient;
+    quotient = x/bin_size;
+    int column_index =(int) quotient;
+
+    quotient = y/bin_size;
+    int row_index=(int)quotient;
+    //   std::cout << column_index + lda* row_index << "\n";
+    return column_index + lda* row_index;
+
+}
+
 // Integrate the ODE
 void move(particle_t& p, double size) {
     // Slightly simplified Velocity Verlet integration
     // Conserves energy better than explicit Euler method
+
+    int origin_bin = calculate_bin_number(p.x, p.y, size, bin_size, lda);
     p.vx += p.ax * dt;
     p.vy += p.ay * dt;
     p.x += p.vx * dt;
@@ -49,28 +62,28 @@ void move(particle_t& p, double size) {
         p.y = p.y < 0 ? -p.y : 2 * size - p.y;
         p.vy = -p.vy;
     }
+
+    int new_bin =calculate_bin_number(p.x, p.y, size, bin_size, lda);
+    if(origin_bin == new_bin) return;
+
+    bins[origin_bin].erase(&p);
+    bins[new_bin].insert(&p);
 }
 
-int calculate_bin_number(double x, double y, double size, double bin_size, int lda){
-    double quotient;
-    quotient = x/bin_size;
-    int column_index =(int) quotient;
-
-    quotient = y/bin_size;
-    int row_index=(int)quotient;
-    //   std::cout << column_index + lda* row_index << "\n";
-    return column_index + lda* row_index;
-
-}
 
 
 void init_simulation(particle_t* parts, int num_parts, double size) {
     double quotient= size/bin_size;
     lda = (int) ceil(quotient);
     int index;
+    const int space = ceil(1.5 * bin_size * bin_size * 1. / density);
+    for(int i = 0; i< lda*lda; ++i){
+        bins[i].reserve(space);
+    }
+
     for (int i = 0; i < num_parts; ++i){
         index = calculate_bin_number(parts[i].x,parts[i].y, size, bin_size,lda);
-        bins[index].insert(i);
+        bins[index].insert(&parts[i]);
     }
     // You can use this space to initialize static, global data objects
     // that you may need. This function will be called once before the
@@ -86,14 +99,14 @@ bool check_boundary(int row , int column){
     }
     return true;
 }
-void apply_force_bin(int row, int column, int p, particle_t* parts, int lda){
+
+void apply_force_bin(int row, int column, particle_t* p, int lda){
     if (not check_boundary(row, column)){
         return;
     }
     for (auto it = bins[column+row*lda].begin(); it != bins[column+row*lda].end(); ++it){
-        apply_force(parts[p], parts[*it]);
+        apply_force(*p, **it);
     }
-
 }
 
 void simulate_one_step(particle_t* parts, int num_parts, double size) {
@@ -104,36 +117,27 @@ void simulate_one_step(particle_t* parts, int num_parts, double size) {
     //         apply_force(parts[i], parts[j]);
     //     }
     // }
-
+    for(int i = 0; i < num_parts; ++i){
+        parts[i].ax = parts[i].ay = 0;
+    }
     for(int i = 0; i < lda; ++i){
         for (int j = 0; j < lda; ++j){
             for (auto it = bins[i+j*lda].begin(); it != bins[i+j*lda].end(); ++it){
-                parts[*it].ax = parts[*it].ay = 0;
-                apply_force_bin(j-1,i-1, *it, parts, lda);
-                apply_force_bin(j-1,i, *it, parts, lda);
-                apply_force_bin(j-1,i+1, *it, parts, lda);
-                apply_force_bin(j,i-1, *it, parts, lda);
-                apply_force_bin(j,i, *it, parts, lda);
-                apply_force_bin(j,i+1, *it, parts, lda);
-                apply_force_bin(j+1,i-1, *it, parts, lda);
-                apply_force_bin(j+1,i, *it, parts, lda);
-                apply_force_bin(j+1,i+1, *it, parts, lda);
+                apply_force_bin(j-1,i-1, *it, lda);
+                apply_force_bin(j-1,i, *it, lda);
+                apply_force_bin(j-1,i+1, *it, lda);
+                apply_force_bin(j,i-1, *it, lda);
+                apply_force_bin(j,i, *it,  lda);
+                apply_force_bin(j,i+1, *it, lda);
+                apply_force_bin(j+1,i-1, *it,  lda);
+                apply_force_bin(j+1,i, *it, lda);
+                apply_force_bin(j+1,i+1, *it,  lda);
             }
         }
     }
-
-
     // Move Particles
     for (int i = 0; i < num_parts; ++i) {
         move(parts[i], size);
     }
 
-    for( int i=0; i< lda*lda;i++){
-        bins[i].clear();
-    }
-    int index;
-    for (int i = 0; i < num_parts; ++i){
-        index = calculate_bin_number(parts[i].x,parts[i].y, size, bin_size, lda);
-        bins[index].insert(i);
-    }
 }
